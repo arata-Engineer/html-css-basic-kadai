58e14853c879a5f3b42e18f3e55c223309931037
arata_Engineer <arataspace1@gmail.com>
2024-07-23 19:55:40 +0900

    kadai_mdn_js01

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,12 @@
+// 本日の日付を取得する
+window.onload = function () {
+    var today = new Date();
+    console.log(today);
+
+
+// 年月日を取得する
+console.log("年=" + today.getFullYear());
+console.log("月=" + (today.getMonth()+1));
+console.log("日=" + today.getDate());
+
+}
